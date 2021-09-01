47ff7c15ca848b855f26bd79cdd3eaec4ae5a3c7
Adam Berger <adam@ratchetdesigns.com>
2021-09-01 10:11:57 -0400

    style: cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,29 @@
 extern crate proc_macro;
 
-use std::fmt::{Display, Formatter, Result as FmtResult};
-use std::collections::{HashSet, HashMap, hash_map::Entry};
-use std::convert::From;
-use std::fs::File;
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
-use syn::{parse, parse2, parenthesized,
-          ItemEnum, Attribute, Meta, MetaList, NestedMeta, Path, Variant, Ident, Token, Fields, Type,
-          punctuated::Punctuated,
-          parse::{Error as ParseError, Parse, ParseStream, Result as ParseResult},
-          visit::{Visit, visit_item_enum}};
-use quote::{quote, format_ident};
+use quote::{format_ident, quote};
+use std::collections::{hash_map::Entry, HashMap, HashSet};
+use std::convert::From;
+use std::fmt::{Display, Formatter, Result as FmtResult};
+use std::fs::File;
+use syn::{
+    parenthesized, parse,
+    parse::{Error as ParseError, Parse, ParseStream, Result as ParseResult},
+    parse2,
+    punctuated::Punctuated,
+    visit::{visit_item_enum, Visit},
+    Attribute, Fields, Ident, ItemEnum, Meta, MetaList, NestedMeta, Path, Token, Type, Variant,
+};
 
 #[proc_macro_derive(FromEnum, attributes(from_enum, from_case))]
 pub fn derive_enum_from(input: TokenStream) -> TokenStream {
-    let result = from_enum_internal(input)
-        .unwrap_or_else(|_err| {
-            panic!("Failed to parse input");
-        });
+    let result = from_enum_internal(input).unwrap_or_else(|_err| {
+        panic!("Failed to parse input");
+    });
 
     let mut file = File::create("output.rs").expect("failed to create file");
-    std::io::Write::write_all(&mut file, result.to_string().as_bytes())
-        .expect("failed to write");
+    std::io::Write::write_all(&mut file, result.to_string().as_bytes()).expect("failed to write");
 
     result
 }
@@ -38,34 +39,26 @@ impl ConversionCfg {
         match &self.dest.fields {
             Fields::Unit => quote! {},
             Fields::Named(named) => {
-                let args = named.named
+                let args = named
+                    .named
                     .iter()
-                    .map(|field| {
-                        xform(
-                            field.ident.as_ref().unwrap(),
-                            &field.ty
-                        )
-                    });
+                    .map(|field| xform(field.ident.as_ref().unwrap(), &field.ty));
 
                 quote! {
                     #(#args),*
                 }
-            },
+            }
             Fields::Unnamed(unnamed) => {
-                let args = unnamed.unnamed
+                let args = unnamed
+                    .unnamed
                     .iter()
                     .enumerate()
-                    .map(|(i, field)| {
-                        xform(
-                            &format_ident!("arg{}", i),
-                            &field.ty
-                        )
-                    });
+                    .map(|(i, field)| xform(&format_ident!("arg{}", i), &field.ty));
 
                 quote! {
                     #(#args),*
                 }
-            },
+            }
         }
     }
 
@@ -78,12 +71,12 @@ impl ConversionCfg {
                 quote! {
                     { #args }
                 }
-            },
+            }
             Fields::Unnamed(_) => {
                 quote! {
                     (#args)
                 }
-            },
+            }
         }
     }
 
@@ -91,7 +84,7 @@ impl ConversionCfg {
         let dest_case = &self.dest.ident;
         let fields = &self.dest.fields;
         let try_type = if use_try_from {
-            quote! { }
+            quote! {}
         } else {
             quote! { .into() }
         };
@@ -99,30 +92,34 @@ impl ConversionCfg {
         match (fields, use_try_from) {
             (Fields::Unit, true) => {
                 panic!("multiple source options found for a single destination and the source does not have a field to try_from");
-            },
+            }
             (Fields::Unit, false) => {
                 quote! {
                     #dest::#dest_case
                 }
-            },
+            }
             (Fields::Named(_), _) => {
-                let args = self.to_wrapped_args(|id| quote! {
-                    #id: #id#try_type
+                let args = self.to_wrapped_args(|id| {
+                    quote! {
+                        #id: #id#try_type
+                    }
                 });
 
                 quote! {
                     #dest::#dest_case #args
                 }
-            },
+            }
             (Fields::Unnamed(_), _) => {
-                let args = self.to_wrapped_args(|id| quote! {
-                    #id#try_type
+                let args = self.to_wrapped_args(|id| {
+                    quote! {
+                        #id#try_type
+                    }
                 });
 
                 quote! {
                     #dest::#dest_case #args
                 }
-            },
+            }
         }
     }
 }
@@ -136,7 +133,7 @@ struct SrcCase {
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum SrcEnum {
     All(),
-    Single(Path)
+    Single(Path),
 }
 
 type SrcCasesBySrc = HashMap<SrcEnum, Vec<SrcCase>>;
@@ -154,17 +151,14 @@ trait MergeIn {
 
 impl<K: Eq + std::hash::Hash, Vs: MergeIn> MergeIn for HashMap<K, Vs> {
     fn merge_in(&mut self, other: Self) {
-        other.into_iter()
-            .for_each(|(k, vs)| {
-                match self.entry(k) {
-                    Entry::Occupied(mut occ) => {
-                        occ.get_mut().merge_in(vs);
-                    },
-                    Entry::Vacant(vac) => {
-                        vac.insert(vs);
-                    },
-                }
-            });
+        other.into_iter().for_each(|(k, vs)| match self.entry(k) {
+            Entry::Occupied(mut occ) => {
+                occ.get_mut().merge_in(vs);
+            }
+            Entry::Vacant(vac) => {
+                vac.insert(vs);
+            }
+        });
     }
 }
 
@@ -202,7 +196,8 @@ impl<'ast> EnumParser {
                             return Default::default();
                         }
 
-                        nested.into_iter()
+                        nested
+                            .into_iter()
                             .filter_map(|m| match m {
                                 NestedMeta::Meta(m) => match m {
                                     Meta::Path(p) => Some(p),
@@ -211,73 +206,72 @@ impl<'ast> EnumParser {
                                 _ => None,
                             })
                             .collect()
-                    },
-                    _ => Default::default()
+                    }
+                    _ => Default::default(),
                 })
-                .unwrap_or_default()
+                .unwrap_or_default(),
         );
     }
 
     fn parse_from_case_attrs(&self, attrs: &'ast Vec<Attribute>) -> SrcCasesBySrc {
-        attrs.iter()
-            .fold(
-                HashMap::new(),
-                |mut m, attr| {
-                    if let Ok(new_attrs) = parse2::<FromCaseAttr>(attr.tokens.clone().into()) {
-                        let new_src_cases_by_src = new_attrs.into_src_cases_by_src();
-                        let known_srcs = new_src_cases_by_src.iter()
-                            .all(|(src_enum, _)| match src_enum {
-                                SrcEnum::All() => true,
-                                SrcEnum::Single(ref src_enum) => self.src_names.contains(src_enum),
-                            });
-                        if !known_srcs {
-                            panic!("Unknown source enum");
-                        }
-
-                        m.merge_in(new_src_cases_by_src);
-                    }
-
-                    m
+        attrs.iter().fold(HashMap::new(), |mut m, attr| {
+            if let Ok(new_attrs) = parse2::<FromCaseAttr>(attr.tokens.clone().into()) {
+                let new_src_cases_by_src = new_attrs.into_src_cases_by_src();
+                let known_srcs = new_src_cases_by_src
+                    .iter()
+                    .all(|(src_enum, _)| match src_enum {
+                        SrcEnum::All() => true,
+                        SrcEnum::Single(ref src_enum) => self.src_names.contains(src_enum),
+                    });
+                if !known_srcs {
+                    panic!("Unknown source enum");
                 }
-            )
+
+                m.merge_in(new_src_cases_by_src);
+            }
+
+            m
+        })
     }
 
-    fn conversion_cfgs_by_src_case_by_src(&self) -> HashMap<Path, HashMap<Ident, Vec<ConversionCfg>>> {
+    fn conversion_cfgs_by_src_case_by_src(
+        &self,
+    ) -> HashMap<Path, HashMap<Ident, Vec<ConversionCfg>>> {
         let src_names = &self.src_names;
 
-        self.src_cases_by_src_by_dest
-            .iter()
-            .fold(
-                HashMap::new(),
-                |mut conversion_cfgs_by_src_case_by_src, (dest, src_cases_by_src)| {
-                    src_cases_by_src.iter()
-                        .for_each(|(src, src_cases)| {
-                            let mut m: HashMap<Path, HashMap<Ident, Vec<ConversionCfg>>> = HashMap::new();
-                            let conversion_cfgs_by_src_case = src_cases.iter()
-                                .map(|src_case| (
-                                    src_case.case_name.clone(),
-                                    vec![ConversionCfg {
-                                        src_case: src_case.clone(),
-                                        dest: dest.clone()
-                                    }]
-                                ))
-                                .collect();
+        self.src_cases_by_src_by_dest.iter().fold(
+            HashMap::new(),
+            |mut conversion_cfgs_by_src_case_by_src, (dest, src_cases_by_src)| {
+                src_cases_by_src.iter().for_each(|(src, src_cases)| {
+                    let mut m: HashMap<Path, HashMap<Ident, Vec<ConversionCfg>>> = HashMap::new();
+                    let conversion_cfgs_by_src_case = src_cases
+                        .iter()
+                        .map(|src_case| {
+                            (
+                                src_case.case_name.clone(),
+                                vec![ConversionCfg {
+                                    src_case: src_case.clone(),
+                                    dest: dest.clone(),
+                                }],
+                            )
+                        })
+                        .collect();
 
-                            match src {
-                                SrcEnum::Single(src) => {
-                                    m.insert(src.clone(), conversion_cfgs_by_src_case);
-                                },
-                                SrcEnum::All() => {
-                                    for src in src_names {
-                                        m.insert(src.clone(), conversion_cfgs_by_src_case.clone());
-                                    }
-                                },
-                            };
-                            conversion_cfgs_by_src_case_by_src.merge_in(m);
-                        });
-                    conversion_cfgs_by_src_case_by_src
-                }
-            )
+                    match src {
+                        SrcEnum::Single(src) => {
+                            m.insert(src.clone(), conversion_cfgs_by_src_case);
+                        }
+                        SrcEnum::All() => {
+                            for src in src_names {
+                                m.insert(src.clone(), conversion_cfgs_by_src_case.clone());
+                            }
+                        }
+                    };
+                    conversion_cfgs_by_src_case_by_src.merge_in(m);
+                });
+                conversion_cfgs_by_src_case_by_src
+            },
+        )
     }
 }
 
@@ -294,13 +288,14 @@ impl<'ast> Visit<'ast> for EnumParser {
                 vec![SrcCase {
                     case_name: node.ident.clone(),
                     fallible: false,
-                }]
+                }],
             );
         }
 
         let mut src_cases_by_src_by_dest = HashMap::new();
         src_cases_by_src_by_dest.insert(node.clone(), src_cases_by_src);
-        self.src_cases_by_src_by_dest.merge_in(src_cases_by_src_by_dest);
+        self.src_cases_by_src_by_dest
+            .merge_in(src_cases_by_src_by_dest);
     }
 }
 
@@ -313,17 +308,16 @@ struct CaseMatch {
 impl CaseMatch {
     fn into_src_cases_by_src(self) -> SrcCasesBySrc {
         let mut scbs = HashMap::new();
-        scbs.insert(
-            self.src_enum,
-            vec![self.src_case],
-        );
+        scbs.insert(self.src_enum, vec![self.src_case]);
         scbs
     }
 }
 
 impl Parse for CaseMatch {
     fn parse(input: ParseStream) -> ParseResult<Self> {
-        let parts: Vec<Path> = Punctuated::<Path, Token![=]>::parse_separated_nonempty(input)?.into_iter().collect();
+        let parts: Vec<Path> = Punctuated::<Path, Token![=]>::parse_separated_nonempty(input)?
+            .into_iter()
+            .collect();
         let parts: &[Path] = &parts;
 
         match parts {
@@ -332,16 +326,19 @@ impl Parse for CaseMatch {
                 src_case: SrcCase {
                     case_name: src_case.get_ident().unwrap().clone(),
                     fallible: false,
-                }
+                },
             }),
             [src_case] => Ok(CaseMatch {
                 src_enum: SrcEnum::All(),
                 src_case: SrcCase {
                     case_name: src_case.get_ident().unwrap().clone(),
                     fallible: false,
-                }
+                },
             }),
-            _ => Err(ParseError::new(input.span(), "Expected #[from_enum(SrcCase, ..)] or #[from_enum(SrcEnum = SrcCase)]")),
+            _ => Err(ParseError::new(
+                input.span(),
+                "Expected #[from_enum(SrcCase, ..)] or #[from_enum(SrcEnum = SrcCase)]",
+            )),
         }
     }
 }
@@ -355,13 +352,10 @@ impl FromCaseAttr {
     fn into_src_cases_by_src(self) -> SrcCasesBySrc {
         self.case_matches
             .into_iter()
-            .fold(
-                HashMap::new(),
-                |mut m, cm| {
-                    m.merge_in(cm.into_src_cases_by_src());
-                    m
-                }
-            )
+            .fold(HashMap::new(), |mut m, cm| {
+                m.merge_in(cm.into_src_cases_by_src());
+                m
+            })
     }
 }
 
@@ -387,64 +381,65 @@ fn from_enum_internal(input: TokenStream) -> Result<TokenStream, Error> {
     let dest = &enm.ident;
     let conversion_cfgs_by_src_case_by_src = parser.conversion_cfgs_by_src_case_by_src();
 
-    let impls = conversion_cfgs_by_src_case_by_src
-        .iter()
-        .map(|(src_name, conversion_cfgs_by_src_case)| {
-            let cases = conversion_cfgs_by_src_case.iter()
-                .map(|(case, conversion_cfgs)| {
-                    let use_try_from = conversion_cfgs.len() > 1;
-                    let conversions = conversion_cfgs.iter()
-                        .map(|conversion_cfg| {
-                            conversion_cfg.to_case_match(dest, use_try_from)
-                        });
-                    let example_conversion_cfg = conversion_cfgs.first()
-                        .unwrap();
-                    let match_result = if use_try_from {
-                        let lhs = example_conversion_cfg
-                            .to_args(|arg, _| quote! { Ok(#arg) });
-                        let rhs = example_conversion_cfg
-                            .to_args(|arg, _| quote! { #arg.try_into() });
-                        let conversions = conversions.map(|c| quote! {
-                            if let (#lhs) = (#rhs) {
-                                return #c;
+    let impls =
+        conversion_cfgs_by_src_case_by_src
+            .iter()
+            .map(|(src_name, conversion_cfgs_by_src_case)| {
+                let cases = conversion_cfgs_by_src_case
+                    .iter()
+                    .map(|(case, conversion_cfgs)| {
+                        let use_try_from = conversion_cfgs.len() > 1;
+                        let conversions = conversion_cfgs
+                            .iter()
+                            .map(|conversion_cfg| conversion_cfg.to_case_match(dest, use_try_from));
+                        let example_conversion_cfg = conversion_cfgs.first().unwrap();
+                        let match_result = if use_try_from {
+                            let lhs = example_conversion_cfg.to_args(|arg, _| quote! { Ok(#arg) });
+                            let rhs =
+                                example_conversion_cfg.to_args(|arg, _| quote! { #arg.try_into() });
+                            let conversions = conversions.map(|c| {
+                                quote! {
+                                    if let (#lhs) = (#rhs) {
+                                        return #c;
+                                    }
+                                }
+                            });
+
+                            quote! {
+                                #(#conversions)*
+                                unreachable!();
                             }
-                        });
+                        } else {
+                            quote! { #(#conversions)* }
+                        };
+
+                        let args = example_conversion_cfg.to_wrapped_args(|arg| quote! { #arg });
 
                         quote! {
-                            #(#conversions)*
-                            unreachable!();
+                            #src_name::#case #args => {
+                                #match_result
+                            }
                         }
-                    } else {
-                        quote! { #(#conversions)* }
-                    };
+                    });
 
-                    let args = example_conversion_cfg
-                        .to_wrapped_args(|arg| quote! { #arg });
+                quote! {
+                    impl std::convert::From<#src_name> for #dest {
+                        fn from(src: #src_name) -> #dest {
+                            use std::convert::Into;
+                            use std::convert::TryInto;
 
-                    quote! {
-                        #src_name::#case #args => {
-                            #match_result
-                        }
-                    }
-                });
-
-            quote! {
-                impl std::convert::From<#src_name> for #dest {
-                    fn from(src: #src_name) -> #dest {
-                        use std::convert::Into;
-                        use std::convert::TryInto;
-
-                        match src {
-                            #(#cases),*
+                            match src {
+                                #(#cases),*
+                            }
                         }
                     }
                 }
-            }
-        });
+            });
 
     Ok(quote! {
         #(#impls)*
-    }.into())
+    }
+    .into())
 }
 
 #[derive(Debug, Clone)]
